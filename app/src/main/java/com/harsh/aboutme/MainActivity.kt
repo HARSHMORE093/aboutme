898d5645699c89a2b15ce3db0ea85944a613cc24
HARSHMORE093 <80156271+HARSHMORE093@users.noreply.github.com>
2021-07-20 16:15:26 +0530

    first commit

---
+++ app/src/main/java/com/harsh/aboutme/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.harsh.aboutme
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.done_button).setOnClickListener{
+            addNickname(it)
+        }
+    }
+
+
+private fun addNickname(view: View){
+    val editText=findViewById<EditText>(R.id.nickname_edit)
+    val nicknametextView=findViewById<TextView>(R.id.nickname_text)
+
+    nicknametextView.text=editText.text
+    editText.visibility=View.GONE   //text hide jaha likh raha the
+    view.visibility=View.GONE         //esse button ko hide kar deya
+    nicknametextView.visibility=View.VISIBLE       //visible ho gaye ga vapis
+
+    // Hide the keyboard.
+    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+}
