69512734c12b72e6118f4a09b018abba3c8e8c46
HARSHMORE093 <80156271+HARSHMORE093@users.noreply.github.com>
2021-07-20 22:20:12 +0530

    second commit

--- app/src/main/java/com/harsh/aboutme/MainActivity.kt
+++ app/src/main/java/com/harsh/aboutme/MainActivity.kt
@@ -4,29 +4,54 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputBinding
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.harsh.aboutme.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val myName:MyName= MyName()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.done_button).setOnClickListener{
+//        setContentView(R.layout.activity_main)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.myname= MyName()
+
+//        findViewById<Button>(R.id.done_button).setOnClickListener{
+//            addNickname(it)
+//        }
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
     }
 
 
 private fun addNickname(view: View){
-    val editText=findViewById<EditText>(R.id.nickname_edit)
-    val nicknametextView=findViewById<TextView>(R.id.nickname_text)
+//    val editText=findViewById<EditText>(R.id.nickname_edit)
+//    val nicknametextView=findViewById<TextView>(R.id.nickname_text)
+//
+//    nicknametextView.text=editText.text
+//    editText.visibility=View.GONE                   //text hide jaha likh raha the
+//    view.visibility=View.GONE                       //esse button ko hide kar deya
+//    nicknametextView.visibility=View.VISIBLE        //visible ho gaye ga vapis
 
-    nicknametextView.text=editText.text
-    editText.visibility=View.GONE   //text hide jaha likh raha the
-    view.visibility=View.GONE         //esse button ko hide kar deya
-    nicknametextView.visibility=View.VISIBLE       //visible ho gaye ga vapis
+
+    binding.apply {
+//        nicknameText.text=binding.nicknameEdit.text
+        myname?.nickname=nicknameEdit.text.toString()
+        invalidateAll()
+        nicknameEdit.visibility=View.GONE
+        doneButton.visibility=View.GONE
+        nicknameText.visibility=View.VISIBLE
+    }
 
     // Hide the keyboard.
     val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -34,3 +59,5 @@ private fun addNickname(view: View){
 }
 
 }
+
+
---
+++ app/src/main/java/com/harsh/aboutme/Myname.kt
@@ -0,0 +1,3 @@
+package com.harsh.aboutme
+
+data class MyName(var name:String="Harsh More",var nickname: String="")
